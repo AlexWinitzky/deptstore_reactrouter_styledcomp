f08a4fde6e662c64729922b2ad74b674e87e7f81
Alex Winitzky <alex.winitzky@gmail.com>
2018-12-11 17:48:33 -0700

    update information on Home.js and About.js

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -2,7 +2,10 @@ import React from 'react';
 import CenterText from './Styles'
 
 const Home = () => (
-  <CenterText tCenter="center">Welcome to Worse Amazon</CenterText>
+  <>
+    <CenterText tCenter="center">Welcome to Worse Amazon</CenterText>
+    <CenterText tCenter="center">These are styled component props altering h1 elements.</CenterText>
+  </>
 )
 
 export default Home
